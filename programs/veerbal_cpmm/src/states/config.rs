9ffe9fd48cea4d847e2a565b8da28ffac206b9c5
veerbal1 <veerbalsingh1@gmail.com>
2025-12-02 11:44:59 +0530

    refactor: Update AmmConfig and PoolState structures for improved clarity and efficiency
    - Changed `index` type from `u16` to `u8` in `AmmConfig`.
    - Rearranged fields in `AmmConfig` and `PoolState` for better organization.
    - Removed unused constants and enums from `pool.rs`.
    - Added `lp_supply` and `creator_fee_active` fields to `PoolState`.
    - Updated `LEN` calculations to utilize `Self::INIT_SPACE` for dynamic sizing.

--- programs/veerbal_cpmm/src/states/config.rs
+++ programs/veerbal_cpmm/src/states/config.rs
@@ -1,39 +1,23 @@
 use anchor_lang::prelude::*;
 
-pub const AMM_CONFIG_SEED: &str = "amm_config";
-
 #[account]
+#[derive(InitSpace)]
 pub struct AmmConfig {
     pub bump: u8,
 
-    pub disable_create_pool: bool,
-
-    pub index: u16,
+    pub index: u8,
 
     pub trade_fee_rate: u64,
-
     pub protocol_fee_rate: u64,
-
     pub fund_fee_rate: u64,
-
+    pub creator_fee_rate: u64,
     pub create_pool_fee: u64,
 
-    pub protocol_owner: Pubkey, 
-
+    pub protocol_owner: Pubkey,
     pub fund_owner: Pubkey,
-
-    pub creator_fee_rate: u64,
-
-    pub padding: [u64; 15]
+    pub disable_create_pool: bool,
 }
 
 impl AmmConfig {
-    pub const LEN: usize = 8 // Discriminator
-    + 1 // bump
-    + 1 // bool
-    + 2 // u16
-    + 8 + 8 + 8 + 8 // u64
-    + 32 + 32 // pubkey
-    + 8 //u64
-    + (8*15);
+    pub const LEN: usize = 8 + Self::INIT_SPACE;
 }
