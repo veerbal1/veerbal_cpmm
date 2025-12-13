bf1c5686ecd63744177940f56d4fa84b135a7d1c
veerbal1 <veerbalsingh1@gmail.com>
2025-12-13 19:19:24 +0530

    Removed react app

--- app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-[#0a0a0f]">
-      <h1 className="text-4xl font-bold text-white">Hello CPMM</h1>
-    </main>
-  );
-}
